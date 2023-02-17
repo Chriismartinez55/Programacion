d3d1c84829f3a8d38295774d403b9757c116ca6f
Chriismartinez55 <124419411+Chriismartinez55@users.noreply.github.com>
2023-02-17 16:41:11 -0400

    Add files via upload

---
+++ ejemplos del libto/Form2.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace ejemplos_del_libto
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prb1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // prb1
+            // 
+            this.prb1.Location = new System.Drawing.Point(83, 64);
+            this.prb1.Name = "prb1";
+            this.prb1.Size = new System.Drawing.Size(140, 23);
+            this.prb1.TabIndex = 0;
+            this.prb1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.prb1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prb1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ejemplos del libto/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemplos_del_libto
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            prb1.Minimum = 1;
+            prb1.Maximum= 100;
+            prb1.Step = 2;
+
+            for (int x=0; x<100; x++)
+            {
+                prb1.PerformStep();
+            }
+
+        
+        }
+    }
+}
