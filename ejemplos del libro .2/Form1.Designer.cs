2c5ac7e8659bb059f5bb61a23c6ff1c50ae926dc
Chriismartinez55 <124419411+Chriismartinez55@users.noreply.github.com>
2023-02-17 17:37:42 -0400

    Add files via upload

---
+++ ejemplos del libro .2/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ejemplos_del_libro._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFrutas = new System.Windows.Forms.ListBox();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.txtFrutas = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstFrutas
+            // 
+            this.lstFrutas.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.lstFrutas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstFrutas.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lstFrutas.FormattingEnabled = true;
+            this.lstFrutas.ItemHeight = 20;
+            this.lstFrutas.Items.AddRange(new object[] {
+            "Manzanas",
+            "Uvas",
+            "Naranjas"});
+            this.lstFrutas.Location = new System.Drawing.Point(12, 97);
+            this.lstFrutas.Name = "lstFrutas";
+            this.lstFrutas.Size = new System.Drawing.Size(225, 204);
+            this.lstFrutas.TabIndex = 0;
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAdicionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdicionar.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.btnAdicionar.Location = new System.Drawing.Point(267, 202);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(161, 52);
+            this.btnAdicionar.TabIndex = 1;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = false;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // txtFrutas
+            // 
+            this.txtFrutas.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtFrutas.Location = new System.Drawing.Point(267, 126);
+            this.txtFrutas.Name = "txtFrutas";
+            this.txtFrutas.Size = new System.Drawing.Size(144, 20);
+            this.txtFrutas.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtFrutas);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.lstFrutas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFrutas;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.TextBox txtFrutas;
+    }
+}
+
---
+++ ejemplos del libro .2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemplos_del_libro._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            // Adicionar Elementos
+
+            lstFrutas.Items.Add("Guineos");
+            lstFrutas.Items.Add("Piñas");
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            lstFrutas.Items.Add(txtFrutas.Text);
+
+            txtFrutas.Text = String.Empty;
+        }
+    }
+}
