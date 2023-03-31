f2291e9f55c27ba80e7429144b6cd3f3577317d6
Chriismartinez55 <124419411+Chriismartinez55@users.noreply.github.com>
2023-03-31 15:38:16 -0400

    Add files via upload

---
+++ Base se datos 1.2/Ejercicios/Ejercicios/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios
+{
+    class programa
+    { 
+        static void Main(string[] args)
+        {
+            double area;
+
+            int radio = 5
+
+                const double pi = 3.1416;
+
+            area = pi * radio * radio;
+
+            Console.WriteLine(area);
+            Console.ReadKey();
+
+            }
+
+            
+        }
+    }
+
